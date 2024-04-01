d7e6b0b1d768a16d9f6217d878cb53dd2c09d7e7
PriyanshuLimbasiya <priyanshulimbasiya812@gmail.com>
2024-04-01 21:10:15 +0530

    New

--- src/app/api-employee.service.ts
+++ src/app/api-employee.service.ts
@@ -2,22 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiEmployeeService {
-  private apiUrl="https://65d5ee0df6967ba8e3bcfd4b.mockapi.io/Employee"
-  constructor(private _http:HttpClient) { }
+  private apiUrl = 'https://65d5ee0df6967ba8e3bcfd4b.mockapi.io/Employee/';
+  constructor(private _http: HttpClient) {}
 
-  getAllEmployee()
-  {
+  getAllEmployee() {
     return this._http.get(this.apiUrl);
   }
-  getById(id:any)
-  {
-    return this._http.get(this.apiUrl+"/"+id)
+  getById(id: any) {
+    return this._http.get(this.apiUrl + id);
   }
-  deleteEmployee()
-  {
-    
-  }
+  deleteEmployee() {}
 }
--- src/app/detailemployee/detailemployee.component.ts
+++ src/app/detailemployee/detailemployee.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { ApiEmployeeService } from '../api-employee.service';
+import { Employee } from '../employee';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detailemployee',
   templateUrl: './detailemployee.component.html',
-  styleUrl: './detailemployee.component.css'
+  styleUrl: './detailemployee.component.css',
 })
 export class DetailemployeeComponent {
-  constructor(private _apiEmployee:ApiEmployeeService){}
+  employeeDetails: any = {};
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _api: ApiEmployeeService
+  ) {}
 
-  ngOnInit(){
-    this._apiEmployee.getById(1).subscribe((res:any)=>{ 
-      // this.data=res;
+  ngOnInit() {
+    let id = this._activatedRoute.snapshot.params['id'];
+    this._api.getById(id).subscribe((res: any) => {
+      this.employeeDetails = res;
+      console.log(this.employeeDetails);
     });
   }
-
 }
